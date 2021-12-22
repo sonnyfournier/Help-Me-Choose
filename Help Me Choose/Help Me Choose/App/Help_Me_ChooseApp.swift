4798437a4db8eb0b9337740ffcdb8fd60de051da
Sonny <sonny.fournier@protonmail.com>
2021-12-22 14:45:49 +0100

    🎉 Create application

---
+++ Help Me Choose/Help Me Choose/App/Help_Me_ChooseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Help_Me_ChooseApp.swift
+//  Help Me Choose
+//
+//  Created by Sonny on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Help_Me_ChooseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
