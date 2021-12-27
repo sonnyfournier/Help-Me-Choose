a45bc2ce4e5ce97de909c7ee2b8d0efe4ab46a5f
Sonny <sonny.fournier@protonmail.com>
2021-12-27 13:56:28 +0100

    ✨ Add weight for spin wheel choices

--- Help Me Choose/Help Me Choose/Views/Fortune Wheel/Wheel/WheelView.swift
+++ Help Me Choose/Help Me Choose/Views/Fortune Wheel/Wheel/WheelView.swift
@@ -11,10 +11,22 @@ struct WheelView: View {
 
     // MARK: Properties
     var labels: [String]
+    var weights: [Double]
     @Binding var selectedIndex: Int?
     private let sliceOffset: Double = -.pi / 2
     private let colors: [Color] = [Color("Wheel1"), Color("Wheel2"), Color("Wheel3"), Color("Wheel4"), Color("Wheel5"),
                                    Color("Wheel6"), Color("Wheel7"), Color("Wheel8"), Color("Wheel9"), Color("Wheel10")]
+
+    // MARK: - Initialization
+    init(labels: [String], weights: [Double]? = nil, selectedIndex: Binding<Int?> = .constant(nil)) {
+        self.labels = labels
+        if let weights = weights {
+            self.weights = weights.map({ Double((Int($0) * 100) / labels.count) })
+        } else {
+            self.weights = (0..<labels.count).map({ _ in Double(100 / labels.count) })
+        }
+        self._selectedIndex = selectedIndex
+    }
 
     // MARK: - Views
     var body: some View {
@@ -25,7 +37,7 @@ struct WheelView: View {
                         .fill(colors[index % colors.count])
                     Text(labels[index]).foregroundColor(Color.white).fontWeight(.bold)
                         .shadow(color: Color("ShadowLight").opacity(0.35), radius: 3, x: 0.0, y: 1.0)
-                        .rotationEffect(textRotation(for: index))
+                        .rotationEffect(textRotation(for: index, in: geo.size))
                         .offset(viewOffset(for: index, in: geo.size)).zIndex(1)
                     WheelCell(startAngle: startAngle(for: index), endAngle: endAngle(for: index))
                         .fill(Color("ShadowDark"))
@@ -42,37 +54,31 @@ struct WheelView: View {
         case 0:
             return sliceOffset
         default:
-            let size = Double(100 / labels.count)
-            let ratio = (size * Double(index)) / (size * Double(labels.count))
-
+            let ratio: Double = weights[..<index].reduce(0.0, +) / weights.reduce(0.0, +)
             return sliceOffset + 2 * .pi * ratio
         }
     }
 
     private func endAngle(for index: Int) -> Double {
         switch index {
-        case labels.count - 1:
+        case weights.count - 1:
             return sliceOffset + 2 * .pi
         default:
-            let size = Double(100 / labels.count)
-            let ratio = (size * Double(index + 1)) / (size * Double(labels.count))
-
+            let ratio: Double = weights[..<(index + 1)].reduce(0.0, +) / weights.reduce(0.0, +)
             return sliceOffset + 2 * .pi * ratio
         }
     }
 
-    private func viewOffset(for index: Int, in geometrySize: CGSize) -> CGSize {
-        let radius = min(geometrySize.width, geometrySize.height) / 3.3
-        let size = Double(100 / labels.count)
-        let dataRatio = (2 * (size * Double(index)) + size) / (2 * (size * Double(labels.count)))
+    private func viewOffset(for index: Int, in size: CGSize) -> CGSize {
+        let radius = min(size.width, size.height) / 3
+        let dataRatio = (2 * weights[..<index].reduce(0, +) + weights[index]) / (2 * weights.reduce(0, +))
         let angle = CGFloat(sliceOffset + 2 * .pi * dataRatio)
 
         return CGSize(width: radius * cos(angle), height: radius * sin(angle))
     }
 
-    private func textRotation(for index: Int) -> Angle {
-        let size = Double(100 / labels.count)
-        let dataRatio = (2 * (size * Double(index)) + size) / (2 * (size * Double(labels.count)))
+    private func textRotation(for index: Int, in size: CGSize) -> Angle {
+        let dataRatio = (2 * weights[..<index].reduce(0, +) + weights[index]) / (2 * weights.reduce(0, +))
         let angle = CGFloat(sliceOffset + 2 * .pi * dataRatio)
 
         return Angle(radians: angle)
@@ -88,6 +94,6 @@ struct WheelView: View {
 struct WheelView_Previews: PreviewProvider {
     static var previews: some View {
         WheelView(labels: ["Paris", "Berlin", "New-York", "Barcelone", "Rome", "Casablanca", "Bruxelles", "Tokyo"],
-                  selectedIndex: .constant(nil))
+                  weights: [1, 1, 2, 3, 1, 1, 2, 1])
     }
 }
